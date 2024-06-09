93e2dbefeb7555540d456a10eae5eeab330793fd
nbo2001 <natanaelbezerra36@gmail.com>
2024-06-09 13:17:57 -0400

    Update charge

--- charge_elastic/charge.py
+++ charge_elastic/charge.py
@@ -59,7 +59,15 @@ index_name = "anime"
 settings = {
     "settings": {
         "number_of_shards": 1,
-        "number_of_replicas": 0
+        "number_of_replicas": 0,
+        "analysis": {
+            "analyzer": {
+                "gen_analyzer": {
+                "tokenizer": "keyword",
+                "filter": [ "word_delimiter", "lowercase" ]
+                }
+            }
+        }
     },
     "mappings": {
         "properties": {
@@ -70,23 +78,31 @@ settings = {
             "main_pic": {"type": "keyword"},
             "type": {"type": "keyword"},
             "source_type": {"type": "keyword"},
-            "num_episodes": {"type": "integer"},
+            "num_episodes": {
+                "type": "rank_feature"
+            },
             "status": {"type": "keyword"},
             "start_date": {"type": "date"},
             "end_date": {"type": "date"},
             "season": {"type": "keyword"},
             "studios": {"type": "keyword"},
-            "genres": {"type": "keyword"},
-            "score": {"type": "float"},
-            "score_count": {"type": "integer"},
+            "genres": {
+                "type": "text",  
+                "analyzer": "gen_analyzer" 
+            },
+            "score": {"type": "rank_feature"},
+            "score_count": {"type": "rank_feature"},
             "score_rank": {"type": "integer"},
             "popularity_rank": {"type": "integer"},
             "members_count": {"type": "integer"},
             "favorites_count": {"type": "integer"},
             "watching_count": {"type": "integer"},
-            "completed_count": {"type": "integer"},
+            "completed_count": {"type": "rank_feature"},
             "on_hold_count": {"type": "integer"},
-            "dropped_count": {"type": "integer"},
+            "dropped_count": {
+                "type": "rank_feature",
+                "positive_score_impact": False
+            },
             "plan_to_watch_count": {"type": "integer"},
             "total_count": {"type": "integer"},
             "score_10_count": {"type": "integer"},
@@ -118,15 +134,30 @@ fisrt = True
 with open(csv_file_path, mode='r', encoding='utf-8') as file:
     csv_reader = csv.DictReader(file, delimiter='\t')
     for row in csv_reader:
+        
         try:
             for field in ['anime_id', 'num_episodes', 'popularity_rank', 'members_count', 'favorites_count', 'watching_count',
                           'completed_count', 'on_hold_count', 'dropped_count', 'plan_to_watch_count', 'total_count',
                           'score_10_count', 'score_09_count', 'score_08_count', 'score_07_count', 'score_06_count',
-                          'score_05_count', 'score_04_count', 'score_03_count', 'score_02_count', 'score_01_count']:
+                          'score_05_count', 'score_04_count', 'score_03_count', 'score_02_count', 'score_01_count', 'score_count', 'score_rank']:
+               
                 if row[field]:
                     row[field] = int(row[field])
                 else:
-                    row[field] = None
+                    row[field] = 0
+
+            for field_rank in ['dropped_count', 'score_count', 'completed_count', 'num_episodes']:
+                if row[field_rank]:
+                    row[field_rank] = int(row[field_rank])
+                    row[field_rank] = row[field_rank] if row[field_rank] > 0 else 1
+                else:
+                    row[field_rank] = 1
+
+            if row['score']:
+                row['score'] = float(row['score'])
+                row['score'] = row['score'] if row['score'] > 0 else 0.1
+            else:
+                row['score'] = 0.1
                     
             # Parse start_date
             if row['start_date']:
@@ -141,6 +172,7 @@ with open(csv_file_path, mode='r', encoding='utf-8') as file:
                 row['end_date'] = None
             
             data.append(row)
+            
         except Exception as e:
             print(f"Error processing row: {e}")
             print(row['start_date'], row['end_date'])
